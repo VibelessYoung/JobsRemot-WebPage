347376b568fd7ea62de75f75c7fd3b36c0866a58
Amir <amirhamidiyan84@gmail.com>
2025-08-31 10:29:59 +0330

    fetch data and add search control

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ const spinnerJobDetailsEl = document.querySelector(".spinner--job-details");
 const submithandler = event =>{
     event.preventDefault();
 
+    jobListSearchEl.innerHTML='';
+
     //GET SEARCH TEXT
     const searchText = searchInputEl.value;
 
@@ -42,17 +44,51 @@ const submithandler = event =>{
     }
 
     searchInputEl.blur();
+
     spinnerSearchEl.classList.add('spinner--visible');
 
+    // fetch('data.json');
+
+    fetch(`https://bytegrad.com/course-assets/js/2/api/jobs?search=${searchText}`)
+        .then(response => {
+            if (!response.ok) {
+                console.log('something went wrong');
+            }
+            
+            return response.json();
+        })
+        .then(data =>{
+            // job items
+            const { jobItems } = data;
+            numberEl.textContent = jobItems.length;
+            console.log(data);
+            spinnerSearchEl.classList.remove('spinner--visible');
+            spinnerSearchEl.classList.add('spinner--hidden');
+
+            jobItems.slice(0,7).forEach(jobItem => {
+                const jobItemHtml = `
+            <li class="job-item">
+                        <a class="job-item__link" href="${jobItem.id}">
+                            <div class="job-item__badge">${jobItem.badgeLetters}</div>
+                            <div class="job-item__middle">
+                                <h3 class="third-heading">${jobItem.title}</h3>
+                                <p class="job-item__company">${jobItem.company}</p>
+                                <div class="job-item__extras">
+                                    <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i>${jobItem.duration}</p>
+                                    <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i>${jobItem.salary}</p>
+                                    <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i>${jobItem.location}</p>
+                                </div>
+                            </div>
+                            <div class="job-item__right">
+                                <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
+                                <time class="job-item__time">${jobItem.daysAgo}d</time>
+                            </div>
+                        </a>
+                    </li>
+            `;
+            jobListSearchEl.insertAdjacentHTML('beforeend', jobItemHtml);
+        });
+        })
+        .catch(error => console.log(error));
 };
-
-fetch("data.json")
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error("مشکل در خواندن فایل JSON:", error);
-  });
-
 searchFormEl.addEventListener('submit' , submithandler);
