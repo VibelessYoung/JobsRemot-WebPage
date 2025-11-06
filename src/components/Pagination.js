e97319f1a5ee912dd7568a7bb196ffb42901af69
Amir <vibelessyoung@gmail.com>
2025-11-06 08:45:06 +0330

    add clickHandler for pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -6,4 +6,12 @@ import {
   paginationNumberBackEl,
   paginationEl,
 } from "../common.js";
-import renderjoblist from "./Joblist";
+import renderjobList from "./JobList.js";
+
+const pagingHandler = (event) => {
+  const clickedButton = event.target.closest(".pagination__button");
+  if (!clickedButton) return;
+  const nextPage = clickedButton.className.includes("--next") ? true : false;
+  alert(nextPage);
+};
+paginationEl.addEventListener("click", pagingHandler);
