9765e38280be1759cf9cf11c62a73b0ba41cd37e
Amir <vibelessyoung@gmail.com>
2025-11-06 08:27:20 +0330

    start create pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,9 @@
+import {
+  state,
+  paginationBtnNextEl,
+  paginationBtnBackEl,
+  paginationNumberNextEl,
+  paginationNumberBackEl,
+  paginationEl,
+} from "../common.js";
+import renderjoblist from "./Joblist";
