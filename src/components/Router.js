1f867b19394add2347db8c97fcc4fab1da3c72b5
Amir <vibelessyoung@gmail.com>
2025-11-07 12:34:51 +0330

    complete router and hash control

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,24 @@
-const loadHandler = () => {
-  console.log(window);
-  console.log(window.history);
-  console.log(window.history);
-  console.log(window.location);
+import { jobDetailsContentEl, BASE_API_URL, getData } from "../common.js";
+import renderSpinner from "./Spinner.js";
+import renderJobDetailsHtml from "./JobDetails.js";
+import renderError from "./Error.js";
+
+const loadHandler = async () => {
+  const id = window.location.hash.substring(1);
+  if (id) {
+    jobDetailsContentEl.innerHTML = "";
+    renderSpinner("job-details");
+    try {
+      const data = await getData(`${BASE_API_URL}/jobs/${id}`);
+
+      const { jobItem } = data;
+
+      renderJobDetailsHtml(jobItem);
+      renderSpinner("job-details");
+    } catch (error) {
+      renderError(error.userError);
+    }
+  }
 };
 window.addEventListener("DOMContentLoaded", loadHandler);
+window.addEventListener("hashchange", loadHandler);
