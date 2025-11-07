6f8b226797fd5b2406fe0ab379553a367ae717e6
Amir <vibelessyoung@gmail.com>
2025-11-07 10:05:12 +0330

    wrote some scripts

---
+++ src/components/Router.js
@@ -0,0 +1,7 @@
+const loadHandler = () => {
+  console.log(window);
+  console.log(window.history);
+  console.log(window.history);
+  console.log(window.location);
+};
+window.addEventListener("DOMContentLoaded", loadHandler);
