55e82e56137bbe5bec6e841f39fa0201f30f0a73
Amir <vibelessyoung@gmail.com>
2025-11-12 10:10:19 +0330

    create dynamic bookmarks

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -1,1 +1,19 @@
-import {} from "";
+import {
+  state,
+  bookmarksBtnEl,
+  jobDetailsEl,
+  jobListBookmarksEl,
+} from "../common.js";
+import renderjobList from "./Joblist.js";
+
+const mouseEnterHandler = () => {
+  bookmarksBtnEl.classList.add("bookmarks-btn--active");
+  jobListBookmarksEl.classList.add("job-list--visible");
+};
+
+const mouseLeaveHandler = () => {
+  bookmarksBtnEl.classList.remove("bookmarks-btn--active");
+  jobListBookmarksEl.classList.remove("job-list--visible");
+};
+bookmarksBtnEl.addEventListener("mouseenter", mouseEnterHandler);
+bookmarksBtnEl.addEventListener("mouseleave", mouseLeaveHandler);
